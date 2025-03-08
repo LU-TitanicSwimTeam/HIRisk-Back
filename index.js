c3dd32f70935bd3a4ab37f1029a64bc6c567367f
rpat9 <rudrap26176@gmail.com>
2025-03-08 13:48:42 -0600

    Installed dependencies like express and cors

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// We will eventually have a html file for a homepage, api pages, and common error pages like 404 or 403. 
 app.get('/', (req, res) => {
     res.send('<h1 style="text-align: center;">Welcome to HIRisk API Homepage!!</h1><p style="text-align: center;">APIs are yet to be implemented for HIRisk.</p>');
 });
 
 app.listen(PORT, () => {
-    console.log(`Backend API running on port ${PORT}`);
+    console.log(`Backend API running on port ${PORT}!`);
 });
