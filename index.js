8ca34327d739dde7a11d26589356978973aca561
Kolade <101372041+marskidris@users.noreply.github.com>
2025-03-10 12:51:20 -0500

    fixed errors that cause site to be blank

--- index.js
+++ index.js
@@ -8,21 +8,20 @@ let heightInches = 70
 
 let weightLBS = 100
 
-let bmi = 0
 
 
 // We will eventually have a html file for a homepage, api pages, and common error pages like 404 or 403. 
 app.get('/', (req, res) => {
-    bmi = (weightLBS / (heightInches * heightInches) * 703)
+    let bmi = (weightLBS / (heightInches * heightInches) * 703); // Declare bmi with let or const
 
-    res.send('<h1 style="text-align: center;">Welcome to HIRisk API Homepage!!</h1><p style="text-align: center;">APIs are yet to be implemented for HIRisk.</p>' +
-       // creating a basic input just to see if it works
-        '<hr/>>' +
-
-        '<p">Height: ${heightInches} inches</p>' +
-        '<p>Weight: ${weightLBS} LBS</p>' +
-        '<p>BMI: ${bmi.toFixed(2)}</p>');
-
+    res.send(`
+        <h1 style="text-align: center;">Welcome to HIRisk API Homepage!!</h1>
+        <p style="text-align: center;">APIs are yet to be implemented for HIRisk.</p>
+        <hr/>
+        <p>Height: ${heightInches} inches</p>
+        <p>Weight: ${weightLBS} LBS</p>
+        <p>BMI: ${bmi.toFixed(2)}</p>
+    `);
 });
 
 app.listen(PORT, () => {
