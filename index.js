1fe222d6b88a2dde83a3f961e35e332cbafb1659
rpat9 <rudrap26176@gmail.com>
2025-03-16 19:57:51 -0500

    Working on S1 (BMI API)

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const express = require('express');
-
-const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-let heightInches = 70
+app.use(express.json());
 
-let weightLBS = 100
-
+// CORS setup
+const FRONTEND_URL = 'https://thankful-sand-02d2aac10.6.azurestaticapps.net';
+app.use(cors({
+    origin: FRONTEND_URL,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 // We will eventually have a html file for a homepage, api pages, and common error pages like 404 or 403. 
 app.get('/', (req, res) => {
-    let bmi = (weightLBS / (heightInches * heightInches) * 703); // Declare bmi with let or const
 
     res.send(`
         <h1 style="text-align: center;">Welcome to HIRisk API Homepage!!</h1>
         <p style="text-align: center;">APIs are yet to be implemented for HIRisk.</p>
-        <hr/>
-        <p>Height: ${heightInches} inches</p>
-        <p>Weight: ${weightLBS} LBS</p>
-        <p>BMI: ${bmi.toFixed(2)}</p>
     `);
 });
+
+
+// bmi calculator
+app.post('/api/calculate-risk', (req, res) =>{
+    const {heightFeet, heightInches, weight} = req.body;
+
+    if(!heightFeet || !heightInches || !weight){
+        return res.status(400).json({error: "Please fill out all fields."});
+    }
+
+    const heightInInches = (parseInt(heightFeet) * 12) + parseInt(heightInches);
+
+    const bmi = (weight / (heightInInches ** 2)) * 703;
+
+    //Dummy Risk Calculation to see if works
+    const riskScore = bmi * 2;
+
+    res.json({ riskScore });
+})
 
 app.listen(PORT, () => {
     console.log(`Backend API running on port ${PORT}!`);
 })
-
-
-// bmi calculator
-
